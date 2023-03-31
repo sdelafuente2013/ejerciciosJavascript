888341e35ad3289de68b088752d33d6e44f08e7b
delafuente <delafuente@tirant.com>
2023-03-31 15:21:11 +0200

    clase 6

--- Clases/clase6.js
+++ Clases/clase6.js
@@ -3,32 +3,32 @@
 // evento para que cargue la pagina
 window.addEventListener('DOMContentLoaded', () => {
     
-    // 1) Guardo mi endpoint en una variable
-    // const cantidadDeGifs = parseInt(prompt('¿Cuantos gifs quieres ver?'))
-    const cantidadDeGifs = 5
-    let busquedaGifs = ''
+    // // 1) Guardo mi endpoint en una variable
+    // // const cantidadDeGifs = parseInt(prompt('¿Cuantos gifs quieres ver?'))
+    // const cantidadDeGifs = 5
+    // let busquedaGifs = ''
 
-    document.querySelector('button').addEventListener('click', () => {
-        busquedaGifs = document.querySelector('input').value
-        console.log(busquedaGifs)
+    // document.querySelector('button').addEventListener('click', () => {
+    //     busquedaGifs = document.querySelector('input').value
+    //     console.log(busquedaGifs)
 
-        let endpointSearch = `https://api.giphy.com/v1/gifs/search?api_key=UsVO5dlZNBAu1lCzqABNN3M8egPwn4in&q=${busquedaGifs}&limit=${cantidadDeGifs}&offset=0&rating=g&lang=en`
+    //     let endpointSearch = `https://api.giphy.com/v1/gifs/search?api_key=UsVO5dlZNBAu1lCzqABNN3M8egPwn4in&q=${busquedaGifs}&limit=${cantidadDeGifs}&offset=0&rating=g&lang=en`
    
-        fetch(endpointSearch)
-            // Convierto la respuesta a un objeto literal de js
-            .then(response => response.json())
-            .then(data => {
-                // eliminar las anteriores imagenes de mi html
-                document.querySelector('.imagenes').innerHTML = ''
+    //     fetch(endpointSearch)
+    //         // Convierto la respuesta a un objeto literal de js
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             // eliminar las anteriores imagenes de mi html
+    //             document.querySelector('.imagenes').innerHTML = ''
 
-                // Recorremos el array de objetos para renderizar la imagen de cada uno  
-                data.data.forEach(element => {
-                    document.querySelector('.imagenes').innerHTML += `<img src="${element.images.original.url}">`
-                })
-            })
-            .catch(error => console.log(error))
-
-        // fetch peticion post
+    //             // Recorremos el array de objetos para renderizar la imagen de cada uno  
+    //             data.data.forEach(element => {
+    //                 document.querySelector('.imagenes').innerHTML += `<img src="${element.images.original.url}">`
+    //             })
+    //         })
+    //         .catch(error => console.log(error))
+    //     })
+    //     // fetch peticion post
         // fetch(endpointSearch, {
         //     method: 'POST',
         //     body: JSON.stringify({
@@ -55,4 +55,6 @@ window.addEventListener('DOMContentLoaded', () => {
     //     })
     //     .catch(error => console.log(error))
 
+    // localStorage
+    // localStorage.setItem('nombre', 'Santiago')
 })
